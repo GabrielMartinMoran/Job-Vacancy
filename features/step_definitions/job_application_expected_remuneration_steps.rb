add9bcc553446a6d058df4adef0cce301581fb45
Mr Kupo <moran.gabriel.95@gmail.com>
2019-10-12 19:05:52 -0300

    German: Created Feature: Expected remuneration on job application

---
+++ features/step_definitions/job_application_expected_remuneration_steps.rb
@@ -0,0 +1,23 @@
+When('I try to apply') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I fill expected remuneration with {int}') do |_int|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I submit application') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('the job offerer receive a mail with application info') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('expected remuneration in mail is {string}') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should see a message indicating error on expected remuneration') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
