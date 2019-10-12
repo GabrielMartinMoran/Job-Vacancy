64f3fd0cc33eb5adaa78eecf6e66b7159c240451
Germán Torós <cybercat_666_78@hotmail.com>
2019-10-12 12:29:29 -0300

    Passing Scenario: Create new offer without tags

--- features/step_definitions/job_offers_tag_steps.rb
+++ features/step_definitions/job_offers_tag_steps.rb
@@ -2,8 +2,10 @@ When('I fill the tags with {string}') do |offer_tags|
   fill_in('job_offer[tags]', with: offer_tags)
 end
 
-Then('I should see {string} in tags field for {string} in My Offers') do |_tags, _offer_name|
-  pending
+Then('I should see {string} in tags field for {string} in My Offers') do |tags, offer_name|
+  page.should have_content('Tags')
+  page.should have_content(tags)
+  page.should have_content(offer_name)
 end
 
 Then('I should see a message indicating that there is too much tags') do
