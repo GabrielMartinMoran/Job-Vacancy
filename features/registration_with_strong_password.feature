1afa531fc099275b04c592d217cb5971be812799
Mr Kupo <moran.gabriel.95@gmail.com>
2019-10-13 12:45:16 -0300

    Passing Scenario: A user tries to register with a strong password

--- features/registration_with_strong_password.feature
+++ features/registration_with_strong_password.feature
@@ -6,7 +6,7 @@ Feature: Registration with strong passwords
   Background:
     Given I access to the register page
 
-  Scenario: a user tries to register with a strong password
+  Scenario: A user tries to register with a strong password
     When I fill password with "A1f43f5%" and password confirmation with "A1f43f5%"
     And I try to register
     Then I should be able to login
--- features/step_definitions/registration_with_strong_password_steps.rb
+++ features/step_definitions/registration_with_strong_password_steps.rb
@@ -2,14 +2,24 @@ Given('I access to the register page') do
   visit '/register'
 end
 
-When('I fill password with {string} and password confirmation with {string}') do |_string, _string2|
-  pending # Write code here that turns the phrase above into concrete actions
+When('I fill password with {string} and password confirmation with {string}') do |password, password_confirmation|
+  @password = password
+  fill_in('user[password]', with: password)
+  fill_in('user[password_confirmation]', with: password_confirmation)
 end
 
 When('I try to register') do
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in('user[name]', with: 'UserName')
+  fill_in('user[email]', with: 'StrongPasswordUser@test.com')
+  fill_in('user[short_bio]', with: 'A' * 50)
+  click_button('Create')
 end
 
 Then('I should be able to login') do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit '/login'
+  fill_in('user[email]', with: 'StrongPasswordUser@test.com')
+  fill_in('user[password]', with: @password)
+  click_button('Login')
+  assert_current_path('/')
+  page.should have_content('StrongPasswordUser@test.com')
 end
