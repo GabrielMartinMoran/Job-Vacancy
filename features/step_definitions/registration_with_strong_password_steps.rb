abac4585db6072b104239076f4522f54db62255c
Mr Kupo <moran.gabriel.95@gmail.com>
2019-10-13 12:39:05 -0300

    Feature Feature registration_with_strong_password

---
+++ features/step_definitions/registration_with_strong_password_steps.rb
@@ -0,0 +1,15 @@
+Given('I access to the register page') do
+  visit '/register'
+end
+
+When('I fill password with {string} and password confirmation with {string}') do |_string, _string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I try to register') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be able to login') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
