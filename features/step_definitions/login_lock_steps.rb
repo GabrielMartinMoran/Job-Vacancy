73be44cf2f0d4c4f2914fcd6e3d16639fe7a5215
Germán Torós <cybercat_666_78@hotmail.com>
2019-10-20 10:34:12 -0300

    Created feature login_lock

---
+++ features/step_definitions/login_lock_steps.rb
@@ -0,0 +1,19 @@
+Given('User is registered with email {string} and password {string}') do |_string, _string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I try to acces with email {string} and wrong password {int} times') do |_string, _int|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('An error indicating that the account is locked should be displayed if user tries to log in') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('User with email {string} is locked') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('{int} hours passed') do |_int|
+  pending # Write code here that turns the phrase above into concrete actions
+end
