63fb4250b910934b68020b28a07b409ca48d497e
Germán Torós <cybercat_666_78@hotmail.com>
2019-10-21 15:36:04 -0300

    Created Given('{string} offer exists in the offers list with tags {string}')

--- features/step_definitions/suggested_offers_on_application.rb
+++ features/step_definitions/suggested_offers_on_application.rb
@@ -1,5 +1,13 @@
-Given('{string} offer exists in the offers list with tags {string}') do |_string, _string2|
-  pending # Write code here that turns the phrase above into concrete actions
+Given('{string} offer exists in the offers list with tags {string}') do |offer, offer_tags|
+  @job_offer = JobOffer.new
+  @job_offer.owner = UserRepository.new.first
+  @job_offer.title = offer
+  @job_offer.location = 'a nice job'
+  @job_offer.description = 'a nice job'
+  @job_offer.tags = offer_tags
+  @job_offer.is_active = true
+
+  JobOfferRepository.new.save @job_offer
 end
 
 When('I apply to {string} offer') do |_string|
