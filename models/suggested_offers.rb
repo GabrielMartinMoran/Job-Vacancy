dd59ddf638295c22de494a74d7ff32a8a6490e9e
Germán Torós <cybercat_666_78@hotmail.com>
2019-10-21 22:00:16 -0300

    SuggestedOffers when obtain should return empty array when none offer match tags

--- models/suggested_offers.rb
+++ models/suggested_offers.rb
@@ -1,5 +1,14 @@
 class SuggestedOffers
+  def initialize(job_offer)
+    @main_offer = job_offer
+    @all_offers = []
+  end
+
   def obtain
     []
+  end
+
+  def add(job_offers)
+    job_offers.each { |job_offer| @all_offers.append(job_offer) }
   end
 end
--- spec/models/suggested_offers_spec.rb
+++ spec/models/suggested_offers_spec.rb
@@ -2,10 +2,18 @@ require 'rspec'
 require_relative '../../models/suggested_offers'
 
 describe SuggestedOffers do
-  subject(:suggested_offers) { described_class.new }
+  subject(:suggested_offers) do
+    described_class.new(JobOffer.new(title: 'a title', tags: 'programmer,web,ruby'))
+  end
 
   context 'when obtain' do
     it 'should return empty array when suggested_offers is created' do
+      expect(suggested_offers.obtain).to eq []
+    end
+
+    it 'should return empty array when none offer match tags' do
+      job_offer = JobOffer.new(title: 'a title', tags: '')
+      suggested_offers.add([job_offer])
       expect(suggested_offers.obtain).to eq []
     end
   end
