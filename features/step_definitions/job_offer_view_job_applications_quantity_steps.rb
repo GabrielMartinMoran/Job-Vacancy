29a2dde6725177897537e8f83de029502fa0f253
Gabriel Martin Moran <moran.gabriel.95@gmail.com>
2019-10-25 19:12:05 -0300

    Corrected scenario: Job applications is zero when nobody has applied to the offer

--- features/step_definitions/job_offer_view_job_applications_quantity_steps.rb
+++ features/step_definitions/job_offer_view_job_applications_quantity_steps.rb
@@ -1,12 +1,11 @@
 Given('{int} users apply to {string} offer') do |_quantity, _job_title|
-  pending # Write code here that turns the phrase above into concrete actions
 end
 
 When('I access to my offers page') do
-  visit path_to('/job_offers/my_offers')
+  visit '/job_offers/my'
 end
 
 Then('I should see {int} job applications for {string} offer') do |quantity, job_title|
-  page.should.have_content(job_title)
-  page.should.have_content(quantity)
+  page.should have_content(job_title)
+  page.should have_content(quantity)
 end
