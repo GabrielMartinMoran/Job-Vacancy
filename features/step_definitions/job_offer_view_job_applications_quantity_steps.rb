8170a968bab51a0f8d9a43d2373816e02cfad05f
Gabriel Martin Moran <moran.gabriel.95@gmail.com>
2019-10-25 18:56:42 -0300

    Implemented step I access to my offers page

--- features/step_definitions/job_offer_view_job_applications_quantity_steps.rb
+++ features/step_definitions/job_offer_view_job_applications_quantity_steps.rb
@@ -3,7 +3,7 @@ Given('{int} users apply to {string} offer') do |_quantity, _job_title|
 end
 
 When('I access to my offers page') do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit path_to('/job_offers/my_offers')
 end
 
 Then('I should see {int} job applications for {string} offer') do |_quantity, _job_title|
