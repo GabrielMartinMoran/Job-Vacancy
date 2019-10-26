3a4c8bb81aeaca6785ee83ef33dc944a39fb248b
Mr Kupo <moran.gabriel.95@gmail.com>
2019-10-26 20:28:41 -0300

    Refactor

--- features/step_definitions/applicants_must_be_logged_to_apply_steps.rb
+++ features/step_definitions/applicants_must_be_logged_to_apply_steps.rb
@@ -1,31 +1,31 @@
-Given("applicant is logged in") do
+Given('applicant is logged in') do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When("applicant tries to apply") do
+When('applicant tries to apply') do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Then("applicant should be in offer apply page") do
+Then('applicant should be in offer apply page') do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Given("applicant is not logged in") do
+Given('applicant is not logged in') do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Then("applicant should be in login page") do
+Then('applicant should be in login page') do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When("applicant logins") do
+When('applicant logins') do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When("applicant registers") do
+When('applicant registers') do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When("applicant navigate to offer apply url") do
+When('applicant navigate to offer apply url') do
   pending # Write code here that turns the phrase above into concrete actions
 end
