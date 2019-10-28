7ba87c417825176446ee888731c745f82b697a1a
Germán Torós <cybercat_666_78@hotmail.com>
2019-10-28 19:28:16 -0300

    Created Feature: Login on register

---
+++ features/step_definitions/login_on_register_steps.rb
@@ -0,0 +1,7 @@
+When('I fill email with {string} and other fields with valid values') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be logged in') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
