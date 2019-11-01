decd45948908098f2cb4886415212d713f891178
Gabriel Martin Moran <moran.gabriel.95@gmail.com>
2019-11-01 18:54:52 -0300

    Added feature user_profile_modification

---
+++ features/step_definitions/user_profile_modification_steps.rb
@@ -0,0 +1,39 @@
+Given('I am logged in as {string}') do |_string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I click on email link') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be in profile update page') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('I am in profile update page') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I update my name, short bio and prefered tags') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I click on save button') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('My profile should be updated') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('My password should be not updated') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I update my password and password confirmation') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('My password should be updated') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
