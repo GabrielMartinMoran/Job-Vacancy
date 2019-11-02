8ed257c8fe996aea717b83ba917b990c65eab706
Gabriel Martin Moran <moran.gabriel.95@gmail.com>
2019-11-02 17:00:58 -0300

    Created feature view_applications_history

---
+++ features/step_definitions/view_applications_history_steps.rb
@@ -0,0 +1,11 @@
+Given('I applied for Job Offer with expected remuneration {float}') do |_expected_remuneration|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I access to my applications') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should see Job Offer application info') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
