63ad748aececefafecd5ef8fec689afc3649c114
Gabriel Martin Moran <moran.gabriel.95@gmail.com>
2019-11-02 17:07:21 -0300

    Implemented step I applied for Job Offer with expected remuneration 10000.0

--- features/step_definitions/view_applications_history_steps.rb
+++ features/step_definitions/view_applications_history_steps.rb
@@ -1,5 +1,7 @@
-Given('I applied for Job Offer with expected remuneration {float}') do |_expected_remuneration|
-  pending # Write code here that turns the phrase above into concrete actions
+Given('I applied for Job Offer with expected remuneration {float}') do |expected_remuneration|
+  visit "/job_offers/apply/#{@job_offer.id}"
+  fill_in('job_application[expected_remuneration]', with: expected_remuneration)
+  click_button('Apply')
 end
 
 When('I access to my applications') do
