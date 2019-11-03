46ec8d67a1538202c069a1560340e021f1f0d05d
Germán Torós <cybercat_666_78@hotmail.com>
2019-11-03 03:07:58 -0300

    Tageable parse_tags should return normalized tags string when raw_tags is provided and max_tags is 3

--- models/tageable.rb
+++ models/tageable.rb
@@ -1,5 +1,7 @@
+require_relative 'tags_normalizer'
+
 module Tageable
-  def parse_tags(_raw_tags)
-    ''
+  def parse_tags(raw_tags, max_tags)
+    TagsNormalizer.new(max_tags).normalize(raw_tags || '')
   end
 end
--- spec/models/tageable_spec.rb
+++ spec/models/tageable_spec.rb
@@ -12,7 +12,11 @@ describe Tageable do
 
   describe 'parse_tags' do
     it 'should return empty string when raw_tags is nil' do
-      expect(tageable_dummy.parse_tags(nil)).to eq ''
+      expect(tageable_dummy.parse_tags(nil, 1)).to eq ''
+    end
+
+    it 'should return normalized tags string when raw_tags is provided and max_tags is 3' do
+      expect(tageable_dummy.parse_tags(' Tag1, tag1, TaG2 , Tag3', 3)).to eq 'tag1,tag2,tag3'
     end
   end
 end
