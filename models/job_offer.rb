0e08dc6c27bb550ac1e220935bbeb133e2fbae88
Germán Torós <cybercat_666_78@hotmail.com>
2019-11-03 03:31:23 -0300

    Refactor JobOffer

--- models/job_offer.rb
+++ models/job_offer.rb
@@ -4,13 +4,12 @@ MAX_TAGS_QUANTITY = 3
 
 class JobOffer
   include ActiveModel::Validations
+  include Taggable
 
   attr_accessor :id, :user, :user_id, :title,
                 :location, :description, :is_active,
                 :updated_on, :created_on, :tags,
                 :applications_quantity, :users_notified
-
-  attr_reader :has_valid_tags
 
   validates :title, presence: true
   def initialize(data = {})
@@ -22,17 +21,9 @@ class JobOffer
     @updated_on = data[:updated_on]
     @created_on = data[:created_on]
     @user_id = data[:user_id]
-    parse_tags(data[:tags])
+    @tags = parse_tags(data[:tags], MAX_TAGS_QUANTITY)
     @applications_quantity = data[:applications_quantity] || 0
     @users_notified = data[:users_notified] || false
-  end
-
-  def parse_tags(tags)
-    @has_valid_tags = true
-    @tags = TagsNormalizer.new(MAX_TAGS_QUANTITY).normalize(tags || '')
-  rescue StandardError
-    @has_valid_tags = false
-    errors.add(:tags, 'Too much tags')
   end
 
   def owner
